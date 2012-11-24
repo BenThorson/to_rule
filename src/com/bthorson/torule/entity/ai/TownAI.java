7f855933bfcc217f8a0b7b7f59ea98c61cc8da9a
benthorson <bthorson79@gmail.com>
2012-11-24 09:38:23 -0600

    Added villagers to town

---
+++ src/com/bthorson/torule/entity/ai/TownAI.java
@@ -0,0 +1,11 @@
+package com.bthorson.torule.entity.ai;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: benthorson
+ * Date: 11/23/12
+ * Time: 11:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TownAI {
+}
---
+++ src/com/bthorson/torule/worldgen/WorldGenParams.java
@@ -0,0 +1,11 @@
+package com.bthorson.torule.worldgen;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: benthorson
+ * Date: 11/18/12
+ * Time: 9:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WorldGenParams {
+}
